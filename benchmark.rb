aab1a51ffd5cce117430cc87b207eabc6a5e843e
Chris Bandy <bandy.chris@gmail.com>
2014-01-07 04:10:06 +0000

    Add some benchmarks to the build

---
+++ benchmark.rb
@@ -0,0 +1,50 @@
+require 'benchmark'
+require 'dci'
+
+class ExampleActor
+  include DCI::Castable
+
+  def regular; end
+end
+
+class ExampleContext
+  include DCI::Context
+
+  def regular; end
+  entry :entry do; end
+
+  entry :with do |actor, &block|
+    cast(actor, :as => ExampleRole)
+    block.call
+  end
+end
+
+module ExampleRole
+  def role; end
+end
+
+actor = ExampleActor.new
+context = ExampleContext.new
+number_of = 10_000
+
+Benchmark.bm(20) do |bm|
+  bm.report('Context method') { number_of.times { context.regular } }
+  bm.report('Context entry point') { number_of.times { context.entry } }
+end
+
+Benchmark.bm(20) do |bm|
+  context.with(actor) do
+    bm.report('Actor method') { number_of.times { actor.regular } }
+    bm.report('Role method') { number_of.times { actor.role } }
+  end
+end
+
+Benchmark.bm(20) do |bm|
+  object = Object.new
+  bm.report('Method missing') { number_of.times { object.missing rescue NoMethodError } }
+  bm.report('Not participating') { number_of.times { actor.missing rescue NoMethodError } }
+
+  context.with(actor) do
+    bm.report('Not implemented') { number_of.times { actor.missing rescue NoMethodError } }
+  end
+end
