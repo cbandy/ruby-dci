b8019e68da7baf6351dd2d578bd7437e6c7dc9fd
Chris Bandy <bandy.chris@gmail.com>
2014-01-15 04:53:57 +0000

    Use __send__ to avoid any naming collisions

--- lib/dci/role_lookup.rb
+++ lib/dci/role_lookup.rb
@@ -7,7 +7,7 @@ module DCI
     # Forward references to constants to the currently executing context
     def const_missing(name)
       context = DCI::Context.current
-      context.respond_to?(name) ? context.send(name) : super
+      context.respond_to?(name) ? context.__send__(name) : super
     end
   end
 end
